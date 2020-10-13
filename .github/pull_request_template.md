331fe60e7af9011240c3eb8426b76a13e3a3e180
Kyle <kwfergus117@gmail.com>
2020-10-13 14:09:24 -0700

    removed junk from DDSA.dart. Added PR template. Implemented Github CI workflow.

---
+++ .github/pull_request_template.md
@@ -0,0 +1,14 @@
+## Data Structure / Algorithm
+
+## Solution / Explanation of Implementation.
+
+
+## Semver Check
+- [ ] **Major** - Backwards Incompatible change on the API (includes removals)
+
+- [ ] **Minor** - Adds or deprecates part of the API.
+
+- [ ] **Patch** - Doesn't effect API
+
+
+#### Tag Reviewer(s)
--- lib/DDSA.dart
+++ lib/DDSA.dart
@@ -1,11 +1,3 @@
 import 'sorting/bubble_sort.dart';
 
-main() {
-  var validList = [3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
-  var invalidList = [
-    Invalid(3), Invalid(1), Invalid(2), Invalid(6), Invalid(4), Invalid(5),
-  ];
-
-  print(bubbleSort(validList));
-  print(bubbleSort<Invalid>(invalidList));
-}
+main() {}
