f4be7a73e5c8b5e00f00169d00a64e23182a8589
Kyle <kwfergus117@gmail.com>
2021-10-13 11:47:03 -0700

    Updated command to include test directory. Added unformated code for Github Action failure QA test.

--- test/ddsa_test.dart
+++ test/ddsa_test.dart
@@ -2,4 +2,6 @@ import 'sorting/sorting_test.dart' as sorting;
 
 void main() {
   sorting.main();
+  // test, delete this before merge.
+  List list = [1, 2, 3, 4, 5, 6, 7 , 8, 9, 1, 2, 3, 4, 5, 6, 7,8 ,9 , 10, 1,2,3,4,5];
 }
