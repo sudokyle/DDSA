a283df22adba9357588149019a4dd8af57a45e89
sunshinesway <61669576+sunshinesway@users.noreply.github.com>
2021-10-16 14:19:45 -0700

    generified linear search algorithm

--- lib/algorithms/searches/linear_search.dart
+++ lib/algorithms/searches/linear_search.dart
@@ -1,6 +1,8 @@
-//for loop to linearly search through list and locate find
-//returns -1 if find not in list, returns index value of find if present in list
-int linearSearch(List<int> list, int find) {
+/// Searches [list] linearly for [find]
+///
+/// For loop to linearly search through [list] and locate [find]
+/// returns -1 if [find] not in [list], returns index value of [find] if present in [list]
+int linearSearch<L>(List<L> list, L find) {
   var index = list.length;
 
   for (var i = 0; i < index; i++) {
@@ -10,16 +12,3 @@ int linearSearch(List<int> list, int find) {
   }
   return -1;
 }
-/*
-void main() {
-  final listTestCases = <List<int>>[
-    [],
-    [1],
-    [1, 5, -100, 50, 30032],
-    [1, 4, 2, 6, 30, 56, 46, 5, 24, 58, 3, 4444],
-  ];
-
-  for (var testCase in listTestCases) {
-    print(linearSearch(testCase, 5));
-  }
-  */
