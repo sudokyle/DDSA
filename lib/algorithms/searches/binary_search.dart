fcf51de93ae1e9086ee63f32722aff2108aa28ec
sunshinesway <61669576+sunshinesway@users.noreply.github.com>
2021-10-18 19:25:23 -0700

    first draft binary search

--- lib/algorithms/searches/binary_search.dart
+++ lib/algorithms/searches/binary_search.dart
@@ -1,29 +1,37 @@
-/// Searches [list] using the binary method for [find]
-/// Algorithm assumes that [list] is in ascending order
-///  search through [list] and locate [find]
-/// returns -1 if [find] not in [list], returns index value of [find] if present in [list]
+/// Searches [list] for [find] using the binary method
+/// This algorithm assumes that [list] is in ascending order
+///
+/// If the midpoint value matches [find], its position in [list] is returned
+/// If the midpoint value is less than [find], the search continues in the lower half of [list].
+/// If the midpoint value is greater than [find], the search continues in the upper half of [list]
+/// In this way, half of [list] is eliminated on every iteration.
+/// -1 is returned if [find] not in [list], returns index value of [find] if present in [list]
+///
 int binarySearch<B extends Comparable>(List<B> list, B find) {
   var numItems = list.length;
   var end = numItems - 1;
   var start = 0;
 
-  for (var i = 0; i < numItems; i++)   {
-    var mid = ((end - start)/2).floor();
-    if (list[i] == find)  {
-      return i;
+  for (var i = 0; i < numItems; i++) {
+    // first compare end of current index list to beginning of index list
+    // to ensure mid variable is not invalid
+    // also automatically ends program if [list] is empty
+    if (start <= end) {
+      var mid = ((end - start)/2).floor();
+        if (list[i] == find) {
+          return i;
+        }
+        // If the midpoint value is less than [find],
+        // the search continues in the lower half of [list]
+          else if (list[mid].compareTo(find) > 0) {
+            end = mid - 1;
+          }
+          //If the midpoint value is greater than [find],
+          // the search continues in the upper half of [list]
+            else {
+              start = mid + 1;
+            }
     }
-    else if (list[mid].compareTo(find) > 0)  {
-      end = mid - 1;
-    }
-    else    {
-      start = mid + 1;
-    }
+    return -1;
   }
-  return -1;
-}
-
-  }
-
-
-
 }
