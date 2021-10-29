17d919f2dd75f25dc25b05d08ad4b265a729b8eb
sunshinesway <61669576+sunshinesway@users.noreply.github.com>
2021-10-29 09:18:11 -0700

    added documentation, added test, added search to ddsa_test

--- lib/algorithms/searches/jump_search.dart
+++ lib/algorithms/searches/jump_search.dart
@@ -4,7 +4,8 @@ import 'dart:math';
 /// Searches [list] for [find] using the jump search method
 /// This algorithm assumes that [list] is in ascending order
 ///
-///
+/// Algorithm searches by jumping items in the list until items are greater than [find]
+/// Then [list] at previous jump point is searched linearly until [find] is found
 /// -1 is returned if [find] not in [list], returns index value of [find] if present in [list]
 
 int jumpSearch<J extends Comparable>(List<J> list, J find) {
@@ -12,6 +13,7 @@ int jumpSearch<J extends Comparable>(List<J> list, J find) {
   var index = 0;
   var jump = sqrt(numItems).floor();
 
+  // Algorithm searches by jumping items in the list until items are greater than find
   while (list[min(jump, numItems) - 1].compareTo(find) < 0) {
     index = jump;
     jump += jump;
@@ -19,6 +21,7 @@ int jumpSearch<J extends Comparable>(List<J> list, J find) {
       return -1;
     }
   }
+  // Then at previous jump point is searched linearly until find is found
   while (list[index].compareTo(find) < 0) {
     index++;
     if (index == min(jump, numItems)) {
