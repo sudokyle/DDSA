49ef368284960c45386b089f41868fcb66148062
Kyle Ferguson <commoncereal@gmail.com>
2022-10-24 16:09:19 -0700

    matrix and vector addition and subtraction and added unit tests.

--- test/ddsa_test.dart
+++ test/ddsa_test.dart
@@ -5,7 +5,7 @@ import 'design_patterns/chain_of_responsibility/chain_of_responsibility_test.dar
 import 'algorithms/searching/linear_search_test.dart' as linear_search;
 import 'algorithms/searching/binary_search_test.dart' as binary_search;
 import 'algorithms/searching/jump_search_test.dart' as jump_search;
-import 'algorithms/mathematics/matrix_multiplication_test.dart' as matrix_multi;
+import 'algorithms/mathematics/matrix_math_test.dart' as matrix_multi;
 import 'data_structures/multi_isolating/miso_test.dart' as miso;
 
 void main() {
